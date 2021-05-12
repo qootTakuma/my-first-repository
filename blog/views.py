638fd2390b1edbc53268a9f04d4934a71a9466f1
qootTakuma <obara.takuma1016@gmail.com>
2021-05-12 19:25:38 +0900

    Modified html file.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+# post_list.htmlを表示するが、Post内の情報を変数postsに格納してhtmlファイル上で動的に呼び出す
+# この例では、今日の日付でPost内に入っているものを出された順に並び替え
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
